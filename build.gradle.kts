8ad55dc0bd10677c56a2c9bf4f0abfa94b7f4580
trannie91 <ngoctrang1091@gmail.com>
2026-09-18 22:07:04 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:v2.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
