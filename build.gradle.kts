27d6693964e5f40fd59712fd271ac5efd8b52dd3
trannie91 <ngoctrang1091@gmail.com>
2026-09-18 21:16:18 +0700

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "cloudstream")
+
+configure<com.android.build.gradle.LibraryExtension> {
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+configure<CloudstreamExtension> {
+    // Tự động nhận diện repo trên GitHub Actions
+    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/your-github-username/blvietsub-cloudstream")
+    authors = listOf("BLVietsub")
+    description = "Xem phim Đam Mỹ BLVietsub mượt mà trên Cloudstream Android TV & Mobile"
+}
+
+dependencies {
+    val cloudstreamApiVersion = "pre-release"
+    implementation("com.github.recloudstream:cloudstream:$cloudstreamApiVersion")
+    implementation("org.jsoup:jsoup:1.17.2")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+rootProject.name = "BLVietsub-Cloudstream"
