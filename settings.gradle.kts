31444d42394cda824074002a062860533f02ca4b
trannie91 <ngoctrang1091@gmail.com>
2026-09-24 21:17:53 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,14 +7,5 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
 rootProject.name = "BLVietsubExtension"
 include(":BLVietsub")
