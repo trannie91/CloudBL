ab38a73dad747eba201128b8c61f3452d9525213
trannie91 <ngoctrang1091@gmail.com>
2026-09-24 22:06:29 +0700

    Update build.gradle.kts

--- BLVietsub/build.gradle.kts
+++ BLVietsub/build.gradle.kts
@@ -3,8 +3,8 @@ android {
 }
 
 dependencies {
-    val cloudstreamApiVersion = "pre-release"
-    compileOnly("com.lagradost:cloudstream3:$cloudstreamApiVersion")
+    // Thư viện Cloudstream core chính thức từ recloudstream
+    compileOnly("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
     compileOnly("org.jsoup:jsoup:1.17.2")
     compileOnly("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
 }
