d3eef2f1b21b0a02efea906e0d7dc1cb052f7ab7
trannie91 <ngoctrang1091@gmail.com>
2026-09-24 21:46:07 +0700

    Update build.gradle.kts

--- BLVietsub/build.gradle.kts
+++ BLVietsub/build.gradle.kts
@@ -1,24 +1,3 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("com.lagradost.cloudstream3.gradle")
-}
-
-cloudstream {
-    setPlugin(
-        name = "BLVietsub",
-        description = "Kho phim Đam Mỹ BLVietsub Mới Cập Nhật - Duy nhất nguồn BLVietsub chuẩn TV Box.",
-        authors = listOf("BLVietsub"),
-        types = listOf(
-            com.lagradost.cloudstream3.TvType.TvSeries,
-            com.lagradost.cloudstream3.TvType.Movie,
-            com.lagradost.cloudstream3.TvType.AsianDrama
-        ),
-        iconUrl = "https://blvietsub.com/wp-content/uploads/2021/08/cropped-favicon-32x32.png",
-        status = com.lagradost.cloudstream3.gradle.CloudstreamExtension.Status.Online
-    )
-}
-
 android {
     namespace = "com.blvietsub"
 }
